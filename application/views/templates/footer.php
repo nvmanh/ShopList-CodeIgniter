b7809628d92b950cf6279cc0e03fcf36175dc55c
Hafidzi <h4fidzi@gmail.com>
2015-09-16 13:00:32 +0800

    add new files

---
+++ application/views/templates/footer.php
@@ -0,0 +1,9 @@
+<br>
+<em>&copy; 2015</em>
+
+<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. 
+<?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?>
+</p>
+
+        </body>
+</html>
